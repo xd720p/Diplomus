12cabb2c01b1bf8a9f5cf784a384ca99609dbf2b
xd720p <petr.kozlov@wukkerteam.com>
2017-11-14 23:29:40 +0300

    -In progress

--- app/src/main/java/com/dummystudent/xd720p/diplomus/MainActivity.java
+++ app/src/main/java/com/dummystudent/xd720p/diplomus/MainActivity.java
@@ -1,9 +1,34 @@
 package com.dummystudent.xd720p.diplomus;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.take_photo_btn) Button takePhotoBtn;
+    @BindView(R.id.photo) ImageView photoView;
+    @BindView(R.id.number) TextView numberView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
